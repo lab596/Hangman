8a4a5f2afb28f528d2476017f4525d4de8f01f96
lab596 <86579136+lab596@users.noreply.github.com>
2022-05-28 09:22:05 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,37 +1,47 @@
+import random
 print("Welcome to Hangman!")
+print()
 print ("There is two game modes: single word or sentance")
 inp = int(input ("Type '1' for single word or '2' for sentance"))
+print()
 if inp == 1:
-  print("Welcome to word mode")
-  print("Your goal is to guess the word within 6 guesses")
-  import random
+  print("Welcome to word mode.")
+  print("Your goal is to guess the word within 6 guesses.")
   n = open("nouns.txt","r")
-
   strn = n.readlines()
   r1= random.randint(0,len(strn)-1)
   word = strn[r1]
   word = word[0 : len(word)-1]
-
+  #print(word)
   guesses = 6
   gword = ""
   for i in word:
     gword+="*"
   print(gword)
+  print()
   gword=""
   letters = []
+  tryw = []
   print("Guesses " + str(guesses))
   while guesses>0:
     letter = input("Guess a letter: ")
-
+    print()
     if len(letter)>1:
       print("Guess only one letter.")
+      print()
       continue
     if letter.isdigit():
-      print("Bro what are you doing? I said put a letter")
+      print("Bro what are you doing? I said put a letter.")
+      print()
+      continue
+    if letter in letters or letter in tryw:
+      print("You already guessed that letter!")
+      print("Try a diffrent one.")
+      print()
       continue
     if word.count(letter) > 0:
       num = word.count(letter)
-      print("There are "+ str(num)+" " + letter.capitalize() +"(s)")
+      print("There are "+ str(num)+" " + letter.capitalize() +"(s).")
       letters.append(letter)
       for i in word:
         if i in letters:
@@ -39,27 +49,32 @@ if inp == 1:
         else:
           gword+="*"
       print(gword)
+      print()
       if gword == word:
         print("You guessed the word!")
         break
       gword=""   
     else:
+      tryw.append(letter)
       print("Sorry the word does not have a "+ letter.capitalize())
       guesses-=1
-      print("Guesses " + str(guesses))
+      print("Guesses Left: " + str(guesses))
+      print()
   if guesses == 0:
     print("You ran out of guessses.")
     print("The word was " + word)
 
 elif inp == 2:
+
   print("Welcome to sentance mode.")
-  print("Your goal is to guess the scentance within 6 guesses")
+  print("Your goal is to guess the scentance within 6 guesses.")
   s = open("sentences.txt","r")
   strs = s.readlines()
   r2= random.randint(0,len(strs)-1)
   sentence = strs[r2]
-  print(sentence)
-  sentence = sentence[0 : len(sentence)-1]
+  sentence = sentence[0 : len(sentence)-2]
+  sentence = sentence.lower()
+  #print(sentence)
   guesses = 6
   gsen = ""
   for i in sentence:
@@ -68,18 +83,28 @@ elif inp == 2:
     else:
       gsen+="*"
   print(gsen)
+  print()
   gsen=""
   letterss = []
+  trys = []
   print("Guesses " + str(guesses))
   while guesses>0:
     letter = input("Guess a letter: ")
-
+    print()
     if len(letter)>1:
       print("Guess only one letter.")
+      print()
       continue
     if letter.isdigit():
       print("Bro what are you doing? I said put a letter")
+      print()
       continue
+    if letter in letterss or letter in trys:
+      print("You already guessed that letter!")
+      print("Try a diffrent one.")
+      print()
+      continue
+
     if sentence.count(letter) > 0:
       num = sentence.count(letter)
       print("There are "+ str(num)+" " + letter.capitalize() +"(s)")
@@ -89,20 +114,25 @@ elif inp == 2:
       for i in sentence:
         if i in letterss:
           gsen+=i
+        elif i == " ":
+          gsen+=" "
         else:
           gsen+="*"
       print(gsen)
+      print()
       if gsen == sentence:
-        print("You guessed the word!")
+        print("You guessed the sentence!")
         break
-      gword=""   
+      gsen=""   
     else:
-      print("Sorry the word does not have a "+ letter.capitalize())
+      trys.append(letter)
+      print("Sorry the sentence does not have a "+ letter.capitalize())
       guesses-=1
-      print("Guesses " + str(guesses))
+      print("Guesses Left: " + str(guesses))
+      print()
   if guesses == 0:
     print("You ran out of guessses.")
-    print("The word was " + word)
+    print("The sentence was " + sentence)
   
 else:
   print("Please type 1 or 2.")
